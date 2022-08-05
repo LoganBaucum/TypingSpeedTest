486a2081119b329d371ca0a01e7b1bf74faeeff7
Logan Baucum <33470316+LoganBaucum@users.noreply.github.com>
2022-08-05 18:51:44 -0500

    Added welcome message

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import curses
 from curses import wrapper
+from time import sleep
 
 def main(stdscr):
+    curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_WHITE)
+    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
+    curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
+    
     stdscr.clear()
-    stdscr.addstr("Hellow World!")
+    stdscr.addstr(4, 5, "Welcome to the Typing Speed Test!", curses.color_pair(0))
+    stdscr.addstr(5, 5, "Lets see how fast you can type! PRESS ANY KEY TO CONTINUE", curses.color_pair(0))
     stdscr.refresh()
     stdscr.getkey()
 
